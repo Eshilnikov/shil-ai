e599e6eb404e150af9804efdb71b9478cbdf4d48
Eugene Shilnikov <eshilnikov@gmail.com>
2026-05-16 15:26:26 +0400

    Fonts: swap to Manrope Variable (sans) + JetBrains Mono Variable, drop Lora/Inter. Matches original Astro Micro setup.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,8 +9,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
-        serif: ["Lora", ...defaultTheme.fontFamily.serif],
+        sans: ["'Manrope Variable'", ...defaultTheme.fontFamily.sans],
+        mono: ["'JetBrains Mono Variable'", ...defaultTheme.fontFamily.mono],
       },
     },
   },
